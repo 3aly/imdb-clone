d3c27af5c96d14a0621ff9e5f4655491ab98f464
3aly <77078308+3aly@users.noreply.github.com>
2023-11-30 01:22:10 +0200

    refactor search bar and strict typing

---
+++ src/components/molecules/searchbar/Searchbar.styles.ts
@@ -0,0 +1,26 @@
+import { makeStyles } from "tss-react/mui";
+
+export const useStyles = makeStyles()((theme) => {
+  return {
+    SearchBarStyle: {
+      width: "40%",
+      input: { color: "white" },
+      color: theme.palette.primary.main,
+      "& .MuiOutlinedInput-root": {
+        "& fieldset": {
+          borderWidth: 2,
+          borderRadius: 8,
+          borderStyle: "solid",
+
+          borderColor: theme.palette.primary.main,
+        },
+        "&.Mui-focused fieldset": {
+          borderWidth: 1,
+          borderRadius: 8,
+          borderStyle: "solid",
+          borderColor: theme.palette.primary.main,
+        },
+      },
+    },
+  };
+});
--- src/components/molecules/searchbar/searchbar.tsx
+++ src/components/molecules/searchbar/searchbar.tsx
@@ -1,50 +1,37 @@
-import React, { SetStateAction } from "react";
-
 // import styles from "./searchbar.css";
-import { TextField, InputAdornment } from "@mui/material";
+import { TextField, InputAdornment, Box } from "@mui/material";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { useStyles } from "./Searchbar.styles";
+import { Dispatch, SetStateAction } from "react";
 type SearchBarProps = {
   title?: string;
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  setSearchTerm: Function;
+  setSearchTerm: Dispatch<SetStateAction<string>>;
 };
 
 export const SearchBar = ({ title, setSearchTerm }: SearchBarProps) => {
+  const { classes } = useStyles();
+
   return (
     <>
       <TextField
         fullWidth
+        className={classes.SearchBarStyle}
         label={title}
-        id="fullWidth"
-        color="primary"
-        style={{ width: "40%", marginTop: 2, height: "" }}
-        sx={{
-          input: { color: "white" },
-          borderWidth: 2,
-          borderRadius: 3,
-          borderStyle: "solid",
-
-          borderColor: "primary.main",
-        }}
         InputLabelProps={{
           style: {
-            fontFamily: "DM Sans",
-            fontSize: 16,
-            fontStyle: "normal",
-            fontWeight: 400,
-            // lineHeight: 24,
-            // borderColor: "white",
             color: "white",
           },
         }}
         InputProps={{
           endAdornment: (
             <InputAdornment position="start">
-              <SearchOutlinedIcon color={"primary"} />
+              <Box sx={{ display: { xs: "none", md: "flex" } }}>
+                <SearchOutlinedIcon color={"primary"} fontSize="large" />
+              </Box>
             </InputAdornment>
           ),
         }}
-        onChange={(e) => {
+        onChange={(e: { target: { value: string } }) => {
           setSearchTerm(e.target.value);
         }}
       />
