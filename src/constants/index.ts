888ede6de1cf3490a1c7877fb1bb28f1c109d7f3
3aly <77078308+3aly@users.noreply.github.com>
2023-11-29 22:06:28 +0200

    add costume typography

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,13 +2,31 @@ import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
   typography: {
-    fontFamily: "DM Sans, sans-serif",
+    h6: {
+      fontSize: 24,
+      fontStyle: "normal",
+      fontFamily: "DM Sans",
+      fontWeight: 700,
+    },
+    h5: {
+      fontFamily: "DM Sans",
+      fontSize: 16,
+      fontStyle: "normal",
+      fontWeight: 400,
+    },
+    body1: {
+      fontWeight: 500,
+    },
+    button: {
+      fontStyle: "italic",
+    },
   },
   palette: {
     primary: {
       main: "#FFFFFF",
       light: "#9CA3AF",
       text: "#6B7280",
+      logo: "#BE123C",
     },
     secondary: {
       main: "#000",
