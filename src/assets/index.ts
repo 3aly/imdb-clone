a10a9c4846dc7831c8051a6e1f124da0e2358959
3aly <77078308+3aly@users.noreply.github.com>
2023-11-29 13:04:35 +0200

    adding images and theme

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+type images = {
+  [key: string]: string;
+};
+import imdb from "./images/imdb.png";
+
+import rotten from "./images/rotten.png";
+
+export const IMAGES: images = {
+  imdb,
+  rotten,
+};
---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: "DM Sans, sans-serif",
+  },
+  palette: {
+    primary: {
+      main: "#FFFFFF",
+      light: "#9CA3AF",
+      text: "#6B7280",
+    },
+    secondary: {
+      main: "#000",
+      light: "#111827",
+    },
+  },
+});
