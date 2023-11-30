fd7c10d81365df4c224ec8c319377ee819bf91d1
3aly <77078308+3aly@users.noreply.github.com>
2023-11-30 19:53:26 +0200

    grid styling done

--- src/components/organisms/movies/movies.tsx
+++ src/components/organisms/movies/movies.tsx
@@ -1,60 +1,27 @@
-import React from "react";
-
-import styles from "./Movies.css";
-import { Box, Grid, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import { MoviePoster } from "../../molecules/index ";
-
-type MoviesProps = {
-  prop?: string;
-  MoviesData: [results: []];
-};
+import { MovieType, MoviesProps } from "../../../types";
+import { useStyles } from "./Movies.styles";
 
 export default function Movies({ MoviesData }: MoviesProps) {
+  const { classes } = useStyles();
+
   return (
-    <Grid
-      container
-      // display={"flex"}
-      spacing={{ xs: 2, md: 3, lg: 4 }}
-      sx={{ backgroundColor: "red" }}
-      // direction="row"
-      // justifyContent="flex-start"
-      // alignItems="center"
-    >
-      {MoviesData.map(({ results: page }, index) => {
-        return page.map(
-          (movie: {
-            id: number;
-            original_title: string;
-            poster_path: string;
-            vote_average: number;
-            original_language: string;
-            release_date: string;
-          }) => {
-            return (
-              <Grid
-                // mx={"auto"}
-                item
-                key={movie.id}
-                xs={12}
-                sm={6}
-                md={4}
-                lg={3}
-                sx={{ backgroundColor: "blue" }}
-                alignItems={"center"}
-                justifyContent={"center"}
-                display={"flex"}
-              >
-                <MoviePoster
-                  title={movie.original_title}
-                  posterUrl={movie.poster_path}
-                  rating={movie.vote_average}
-                  lang={movie.original_language}
-                  date={movie.release_date}
-                />
-              </Grid>
-            );
-          }
-        );
+    <Grid container rowGap={4} columnSpacing={15}>
+      {MoviesData.map(({ results: page }) => {
+        return page.map((movie: MovieType) => {
+          return (
+            <Grid item key={movie.id} xs={12} sm={6} md={4} lg={3}>
+              <MoviePoster
+                title={movie.original_title}
+                posterUrl={movie.poster_path}
+                rating={movie.vote_average}
+                lang={movie.original_language}
+                date={movie.release_date}
+              />
+            </Grid>
+          );
+        });
       })}
     </Grid>
   );
