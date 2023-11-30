ab786e947441c9302ecb90009ef9f2fc93c6ee0f
3aly <77078308+3aly@users.noreply.github.com>
2023-11-30 02:13:25 +0200

    grid tweaks

--- src/components/organisms/movies/movies.tsx
+++ src/components/organisms/movies/movies.tsx
@@ -37,7 +37,7 @@ export default function Movies({ MoviesData }: MoviesProps) {
                 key={movie.id}
                 xs={12}
                 sm={6}
-                md={3}
+                md={4}
                 lg={3}
                 sx={{ backgroundColor: "blue" }}
                 alignItems={"center"}
